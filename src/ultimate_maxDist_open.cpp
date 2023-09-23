f39b18a757e063fe6dbf08d3da7e9a1ae0c7a310
Wonder Alexandre Luz Alves <wonderalexandre@gmail.com>
2023-09-23 11:57:38 -0300

    inclusion of the UAO in morphotree

--- src/ultimate_maxDist_open.cpp
+++ src/ultimate_maxDist_open.cpp
@@ -4,12 +4,14 @@
 #include <morphotree/tree/mtree.hpp>
 #include <morphotree/adjacency/adjacency4c.hpp>
 #include <morphotree/adjacency/adjacency8c.hpp>
+#include <morphotree/attributes/areaComputer.hpp>
+#include <morphotree/residues/UltimateAttributeOpening.hpp>
 
 #include <iostream>
 #include <string>
 
 #include <MaxDistComputer.hpp>
-#include <UltimateAttributeOpening.hpp>
+
 
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
@@ -32,6 +34,8 @@ int main(int argc, char *argv[])
   using morphotree::Adjacency;
   using morphotree::buildMaxTree;
   using NodePtr = MorphologicalTree<uint8>::NodePtr;
+  using morphotree::AreaComputer;
+  using morphotree::UltimateAttributeOpening;
 
   // check number of arguments from the command call
   if (argc < 4) {
@@ -39,7 +43,6 @@ int main(int argc, char *argv[])
     std::cerr << "usage: ultimate_alongation_open <image> <out_img> <maxCriterion>\n";
     return -1;
   }
-
   // read image
   int width, height, nchannels;
   uint8 *data = stbi_load(argv[1], &width, &height, &nchannels, 1);
@@ -51,9 +54,13 @@ int main(int argc, char *argv[])
   // build max-tree
   std::shared_ptr<Adjacency> adj = std::make_shared<Adjacency8C>(domain);
   MorphologicalTree<uint8> maxtree = buildMaxTree(f, adj);
-
   std::cout << "Number of nodes: " << maxtree.numberOfNodes() << std::endl;
 
+  /*int tarea = 30;
+  std::vector<uint32> area = AreaComputer<uint8>().computeAttribute(maxtree);
+  maxtree.idirectFilter([&area, tarea](NodePtr node){return area[node->id()] >= tarea;});
+  std::cout << "Number of nodes: " << maxtree.numberOfNodes() << std::endl;
+  */
 
   // attributes
   std::vector<uint32> maxDist = computeMaxDistanceAttribute(domain, f, maxtree);
@@ -70,11 +77,15 @@ int main(int argc, char *argv[])
   std::vector<uint8> imgRGBAssociated = uao.getAssociatedColorImage();
 	std::cout << "computeUAO [OK]" << std::endl;	
   
-        
+  std::string s(argv[2]);
+  s = s.substr(s.find_last_of("/")+1);
+  std::string name = s.substr(0, s.length()-4); 
+  std::string name_maxConstrast = name + "_maxConstrast.png";
+  std::string name_associated = name +  "_associated.png";         
 
   // record filtered image into the disk
-  stbi_write_png(argv[2], domain.width(), domain.height(), 1, imgMaxConstrast.data(), 0);
-  stbi_write_png("imgAssociated.png", domain.width(), domain.height(), 3, imgRGBAssociated.data(), 0);
+  stbi_write_png(name_maxConstrast.c_str(), domain.width(), domain.height(), 1, imgMaxConstrast.data(), 0);
+  stbi_write_png(name_associated.c_str(), domain.width(), domain.height(), 3, imgRGBAssociated.data(), 0);
   stbi_image_free(data);
   
   return 0;
