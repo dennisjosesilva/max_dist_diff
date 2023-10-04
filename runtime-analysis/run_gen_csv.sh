608cce35f367d3844a2c0390a97cb081ad59298f
Dennis José da Silva <dennisjosesilva@gmail.com>
2023-10-04 11:39:00 -0300

    Setting up the runtime experiments.

--- runtime-analysis/run_gen_csv.sh
+++ runtime-analysis/run_gen_csv.sh
@@ -1,11 +1,20 @@
 # https://www.lrde.epita.fr/wiki/Publications/carlinet.14.itippython
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_nd_01.csv -e nd
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_dn_01.csv -e dn
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_nd_02.csv -e nd
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_dn_02.csv -e dn
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_nd_03.csv -e nd
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_dn_03.csv -e dn
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_nd_04.csv -e nd
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_dn_04.csv -e dn
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_nd_05.csv -e nd
+sleep 10
 python gen_csv.py -i ../runtime-dataset/carlinet-14-itip -o csv/runtime_dn_05.csv -e dn
